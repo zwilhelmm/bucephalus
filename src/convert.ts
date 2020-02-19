e7664fd8e53e72797aaa9001e5d2a005c1496b4e
zwilhelmm <13949359+zwilhelmm@users.noreply.github.com>
2020-02-18 23:38:46 -0800

    Add-callback-to-generator

--- src/convert.ts
+++ src/convert.ts
@@ -2,17 +2,35 @@
 
 // Third-party dependencies.
 import _ from "lodash";
+import util from 'util';
 
 interface WrappedFunction extends Function {
   fname?: string;
 }
 
-const callbackToGenerator: Generator<Function, Function, Error> = function(fn: WrappedFunction) {
+const callbackToGenerator = function(fn: WrappedFunction) {
   const args = Array.prototype.slice.call(arguments, 0, arguments.length);
   return {
     fname: fn.fname || fn.name,
+    
     ...function*() {
       return yield fn.apply(null, ...args);
+    }
+  };
+};
+
+const callbackToPromise = function(
+  fn: WrappedFunction
+) {
+  return {
+    fname: fn.fname || fn.name,
+    ...function<CallbackValue>() {
+      const args = [...arguments].slice(0, -1);
+      return new Promise<CallbackValue>((resolve, reject) => {
+        const callbackValue = (err: Error, val: CallbackValue) =>
+          err ? reject(err) : resolve(val);
+        promiseToCallback(fn).apply(null, args.concat([callbackValue]));
+      });
     }
   };
 };
@@ -30,21 +48,7 @@ const promiseToCallback: WrappedFunction = function<CallbackValue>(
   };
 };
 
-const callbackToPromise: Promise<PromisedValue> = function(
-  fn: WrappedFunction
-) {
-  return {
-    fname: fn.fname || fn.name,
-    ...function<CallbackValue>() {
-      const args = [...arguments].slice(0, -1);
-      return new Promise((resolve, reject) => {
-        const callbackValue = (err: Error, val: CallbackValue) =>
-          err ? reject(err) : resolve(val);
-        promiseToCallback(fn).apply(null, args.concat([callbackValue]));
-      });
-    }
-  };
-};
+
 
 module.exports = {
   promiseToCallback,
